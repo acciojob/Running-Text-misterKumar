fcff312384094ac676372e3a94e6c745ead6b39d
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-28 15:21:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
 //your JS code here. If required.
-const textElement = document.getElementById("text");
-const speedInput = document.getElementById("speed");
-const startButton = document.getElementById("startButton");
+const text = document.getElementById('text');
+      const speedInput = document.getElementById('speed');
+      const originalText = 'We love Programming!';
+      let currentIndex = 0;
 
-let intervalId;
+      function typeNextCharacter() {
+        if (currentIndex < originalText.length) {
+          const currentCharacter = originalText.charAt(currentIndex);
+          text.innerHTML += currentCharacter;
+          currentIndex++;
+          setTimeout(typeNextCharacter, 500 / parseInt(speedInput.value));
+        }
+      }
 
-startButton.addEventListener("click", function () {
-    clearInterval(intervalId);
+      speedInput.addEventListener('input', () => {
+        // Reset the text and index when the speed changes
+        text.innerHTML = '';
+        currentIndex = 0;
+        // Start typing again with the new speed
+        typeNextCharacter();
+      });
 
-    const speed = parseInt(speedInput.value);
-    const text = textElement.innerText;
-    textElement.innerText = ''
-    if (speed >= 1 && speed <= 10) {
-        const delay = 500 / speed;
-        let i = 0;
-
-        intervalId = setInterval(function () {
-            if (i < text.length) {
-                textElement.textContent += text[i];
-                i++;
-            } else {
-                clearInterval(intervalId);
-            }
-        }, delay);
-    } else {
-        alert("Please enter a speed between 1 and 10.");
-    }
-});
+      // Start typing the text with the initial speed
+      typeNextCharacter();
+   
